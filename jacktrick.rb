11b8167c03c40e6f2cfe06f9a69205b185e0aa0f
Jack Bracken <contact@jacktrick.com>
2012-11-21 13:38:08 +0000

    Added @font-face CSS rules, added absender font, cleaned structure

--- jacktrick.rb
+++ jacktrick.rb
@@ -2,26 +2,21 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
-  @page = "index"
   haml :index, :locals => {:page => "HOME"}
 end
 
 get '/blog' do
-  @page = "blog"
   haml :blog, :locals => {:page => "BLOG"}
 end
 
 get '/code' do
-  @page = "code"
   haml :code, :locals => {:page => "CODE"}
 end
 
 get '/photo' do
-  @page = "photo"
   haml :photo, :locals => {:page => "PHOTOGRAPHY"}
 end
 
 get '/contact' do
-  @page = "contact"
   haml :contact, :locals => {:page => "CONTACT"}
 end
