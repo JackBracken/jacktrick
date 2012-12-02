1f4d5043309edd74fe5622094d388c058ce40365
Jack Bracken <contact@jacktrick.com>
2012-12-02 22:40:36 +0000

    Added datamapper stuff for blog database. Updated .gitignore to ignore database stuff.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'http://rubygems.org/'
 gem "sinatra", "1.3.3"
 gem "haml", "3.1.7"
+gem "data_mapper"
+gem "dm-sqlite-adapter"
--- jacktrick.rb
+++ jacktrick.rb
@@ -1,11 +1,45 @@
+require 'bundler/setup'
 require 'sinatra'
 require 'haml'
+require 'data_mapper'
+require 'dm-sqlite-adapter'
+require 'time'
+
+DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/jacktrick_test")
+
+class BlogPost
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :title,      String, :required => true, :length => 80
+  property :created_at, DateTime
+  property :body,       Text
+end
+
+# DataMapper.auto_migrate!
+DataMapper.finalize
+
+# Two small test cases
+
+first_post = BlogPost.new
+first_post.title = "Aggabag"
+first_post.body = "I am a blog post thing. LOL! This is the last line??"
+first_post.save
+
+s_post = BlogPost.new
+s_post.title = "The black eyed peas are shit"
+s_post.body = "It's true. Their music is atrocious, like a bunch of retards getting the shit  kicked out of them by cats."
+s_post.save
 
 helpers do
-
+  def convert_date(date)
+    date.strftime("%^b %e, %Y")
+  end
 end
 
 get '/' do
+  @posts = BlogPost.all :limit => 10,
+                        :order => :created_at.desc
   haml :index, :locals => {:page => "INDEX"}
 end
 
