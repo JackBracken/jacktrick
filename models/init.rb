0e7e7cfd4d3c2a887e590a8a558c6fc78f2755ce
Jack Bracken <contact@jacktrick.com>
2012-12-11 20:27:23 +0000

    Disabled popultion route to prevent people messing with the database.

--- models/init.rb
+++ models/init.rb
@@ -5,4 +5,5 @@ DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://localhost/jacktric_site")
 require_relative 'blogpost'
 DataMapper.auto_upgrade!
+# DataMapper.auto_migrate! # deletes contents of database - only for testing, never production
 DataMapper.finalize
