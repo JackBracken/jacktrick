594da547cb8cf73fd8014eb9aa9966ec8df150c6
Jack Bracken <contact@jacktrick.com>
2012-12-30 03:37:53 +0000

    Switching to SQLite

--- Gemfile
+++ Gemfile
@@ -2,16 +2,4 @@ source 'http://rubygems.org/'
 gem "sinatra", "1.3.3"
 gem "haml", "3.1.7"
 gem "data_mapper", "1.2.0"
-
-group :development do
-  gem "dm-mysql-adapter"
-end
-
-group :heroku do
-  gem 'pg'
-  gem 'dm-postgres-adapter'
-end
-
-group :production do#
-  "dm-mysql-adapter"
-end
+gem "sqlite3"
--- models/init.rb
+++ models/init.rb
@@ -2,8 +2,10 @@ require 'data_mapper'
 
 DataMapper::Logger.new($stdout, :debug)
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://localhost/jacktric_site")
+DataMapper.setup(:default, 'sqlite:///db/blog.db')
+
 require_relative 'blogpost'
 DataMapper.auto_upgrade!
 # DataMapper.auto_migrate! # deletes contents of database - only for testing, never production
+
 DataMapper.finalize
