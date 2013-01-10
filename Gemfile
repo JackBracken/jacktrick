de7b5b7cf814a1b32bea6a29d013e333e9f7825d
Jack Bracken <contact@jacktrick.com>
2013-01-10 14:09:10 +0000

    Getting ready for heroku

--- Gemfile
+++ Gemfile
@@ -2,13 +2,14 @@ source 'http://rubygems.org/'
 gem "sinatra", "1.3.3"
 gem "haml", "3.1.7"
 gem "data_mapper", "1.2.0"
-gem "dm-sqlite-adapter"
 gem "nokogiri"
 gem "redcarpet"
 
 group :production do
   gem 'pg'
+  gem 'dm-postgres-adapter'
 end
 group :development, :test do
   gem 'sqlite3'
+  gem "dm-sqlite-adapter"
 end
--- models/init.rb
+++ models/init.rb
@@ -1,15 +1,8 @@
 require 'data_mapper'
-require 'fileutils'
 
-db_dir = "#{Dir.pwd}/db"
-
-FileUtils.mkdir_p db_dir
-FileUtils.touch("#{db_dir}/blog.db") unless File.exists?("#{db_dir}/blog.db") 
-
-DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, "sqlite:///#{Dir.pwd}/db/blog.db")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db/blog.db")
 
 require_relative 'blogpost'
-DataMapper.auto_upgrade!
 
+DataMapper.auto_upgrade!
 DataMapper.finalize
